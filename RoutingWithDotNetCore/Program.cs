64145bb8bfd82ad3153576fd65f580905f241380
zaintahiri <zain.tahiri940@gmail.com>
2024-12-18 20:28:42 +0500

    Map , MapGet, MapPost routing used to redirect client as per request. with above routing methods you can give routing, and middleware to execute. if routing doesnt' match then short circuit middleware will be executed.

--- RoutingWithDotNetCore/Program.cs
+++ RoutingWithDotNetCore/Program.cs
@@ -6,7 +6,28 @@ app.UseRouting();
 
 app.UseEndpoints(endpoints =>
 {
+    endpoints.Map("/Home",async (context) =>
+    {
+        await context.Response.WriteAsync("Hi, you are in HOME page");
+    });
+
+    endpoints.MapGet("/Product", async (context) =>
+    {
+        await context.Response.WriteAsync("Hi, you are in PRODUCT page");
+    });
+
+    endpoints.MapPost("/Product", async (context) =>
+    {
+        await context.Response.WriteAsync("Hi, you have added PRODUCT");
+    });
+
+   
+
+
     // define endpoints here 
 });
 
+app.Run(async(HttpContext contex) => {
+    await contex.Response.WriteAsync("Error code 404, Page not found");
+});
 app.Run();
