7de8b1098e090d28ac4c15bef86b5bfea3cc8b81
zaintahiri <zain.tahiri940@gmail.com>
2024-12-18 21:12:38 +0500

    how to define routing parameters in dotnet core. how to get these parameters and use them in middleware

--- RoutingWithDotNetCore/Program.cs
+++ RoutingWithDotNetCore/Program.cs
@@ -31,7 +31,22 @@ app.UseEndpoints(endpoints =>
         await context.Response.WriteAsync("\nHi, you have added PRODUCT");
     });
 
-   
+    // here is how to define parameters for get request, with two parameters
+    endpoints.MapGet("/Books/{autherName}/{bookId}", async(context) =>
+    {
+        var authername = Convert.ToString(context.Request.RouteValues["autherName"]);
+        var bookId = Convert.ToInt32(context.Request.RouteValues["bookId"]);
+        await context.Response.WriteAsync($"\n\nBook is written by {authername} and book ID is {bookId}");
+
+    });
+
+    // here is how to define parameters for get request, with one parameter
+    endpoints.MapGet("/Books/{bookId}", async (context) =>
+    {
+        var bookId = Convert.ToInt32(context.Request.RouteValues["bookId"]);
+        await context.Response.WriteAsync($"\n\nPurchased book has Book ID {bookId}");
+
+    });
 
 
     // define endpoints here 
