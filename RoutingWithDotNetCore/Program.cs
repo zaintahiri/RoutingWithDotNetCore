ea623027adbac570d99222c1eaf4f25231dad5d2
zaintahiri <zain.tahiri940@gmail.com>
2024-12-18 20:48:38 +0500

    // when you are getting endpoint you must use it After app.UseRouting, otherwise it will return null

--- RoutingWithDotNetCore/Program.cs
+++ RoutingWithDotNetCore/Program.cs
@@ -3,22 +3,32 @@ var app = builder.Build();
 
 // this is used to enable routing in dotnet core
 app.UseRouting();
+// when you are getting endpoint you must use it After app.UseRouting, otherwise it will return null
+app.Use(async (context,next) =>
+{
+    Endpoint endpoint =  context.GetEndpoint();
+    if (endpoint != null)
+    {
+        await context.Response.WriteAsync("\nEndpoint name : " + endpoint.DisplayName);
 
+    }
+    await next(context);
+});
 app.UseEndpoints(endpoints =>
 {
     endpoints.Map("/Home",async (context) =>
     {
-        await context.Response.WriteAsync("Hi, you are in HOME page");
+        await context.Response.WriteAsync("\nHi, you are in HOME page");
     });
 
     endpoints.MapGet("/Product", async (context) =>
     {
-        await context.Response.WriteAsync("Hi, you are in PRODUCT page");
+        await context.Response.WriteAsync("\nHi, you are in PRODUCT page");
     });
 
     endpoints.MapPost("/Product", async (context) =>
     {
-        await context.Response.WriteAsync("Hi, you have added PRODUCT");
+        await context.Response.WriteAsync("\nHi, you have added PRODUCT");
     });
 
    
@@ -28,6 +38,6 @@ app.UseEndpoints(endpoints =>
 });
 
 app.Run(async(HttpContext contex) => {
-    await contex.Response.WriteAsync("Error code 404, Page not found");
+    await contex.Response.WriteAsync("\nError code 404, Page not found");
 });
 app.Run();
